df1f803cf4ed59f3febe3ef1b403a9165d42b877
Afreen <afreensultana567@gmail.com>
2020-12-21 14:53:16 +0530

    java file modified and added

--- Demo.java
+++ Demo.java
@@ -3,5 +3,7 @@ public class Demo{
    public static void main(String args[])
   {
     System.out.println("Welome to the Website");
+   for(int i=0;i<10;i++){
+         System.out.println(i);}
    }
 }
